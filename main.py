de94b0319bc814c4e7653db8bd98f289a335f2ec
ShchuBelka-crypto <tsybulskayanastya@gmail.com>
2026-02-17 00:20:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
 import os
-import asyncio
-from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.environ.get("BOT_TOKEN")
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Bot is running"
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает 🚀")
 
-async def run_bot():
-    application = ApplicationBuilder().token(TOKEN).build()
-    application.add_handler(CommandHandler("start", start))
-    await application.initialize()
-    await application.start()
-    await application.updater.start_polling()
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(run_bot())
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    main()
